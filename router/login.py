283b4457bd0f746a346a521425c9eea2acc259f2
Pich09 <chheangmeng009@gmail.com>
2024-04-09 07:31:21 +0700

    update

--- router/login.py
+++ router/login.py
@@ -18,7 +18,8 @@ async def login(request : OAuth2PasswordRequestForm = Depends(), db : Session = 
     if not user: 
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credential")
     
-    if not psw_ctx.verify(request.password, user.password): #user is from the filter above
+    if not psw_ctx.verify(request.password, user.password): 
+        #user is from the filter above
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Incorrect Password")
     
     access_token = authorization.token.create_access_token(data={"sub": user.email})
